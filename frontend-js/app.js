9fe67d34f0f5ce47f703bcc3a5f6e0f9bf73ebd6
Eszter Csaki <esztercsaki3@gmail.com>
2022-11-22 16:28:31 +0100

    dom shows some info

--- frontend-js/app.js
+++ frontend-js/app.js
@@ -1,26 +1,36 @@
-const button = document.querySelector('button');
 const localhost = 'http://localhost:3000/api';
 const weatherID = '46d4b7c5d34fa20f4e66d522546c5d5f';
 const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
+const button = document.querySelector('button');
+const city = document.querySelector('#search');
+const card = document.querySelector('.card');
+const cardTitle = document.querySelector('.card-title');
+const cardText = document.querySelector('.card-text');
+const textP = document.querySelector('.temp');
+
 button.addEventListener('click', (e) => {
   e.preventDefault();
-  fetch(`${localhost}/weather/Budapest`)
+
+  fetch(`${localhost}/weather/${city.value}`)
     .then((resp) => resp.json())
     .then((json) => {
+      console.log(json);
       if (json.length === 0) {
-        addToDatabase();
+        addToDatabase(city.value);
       } else {
+        appendInfoFromDB(json);
         console.log(json);
       }
     })
     .catch((err) => console.log(err));
 });
 
-function addToDatabase() {
-  fetch(`${weatherUrl}Budapest&appid=${weatherID}&units=metric`)
+function addToDatabase(city) {
+  fetch(`${weatherUrl}${city}&appid=${weatherID}&units=metric`)
     .then((resp) => resp.json())
     .then((json) => {
+      appendInfoFromWeb(json);
       return fetch(`${localhost}/weather`, {
         method: 'POST',
         headers: {
@@ -30,4 +40,16 @@ function addToDatabase() {
       });
     })
     .catch((err) => console.log(err));
+}
+
+function appendInfoFromDB(json) {
+  card.style.display = 'flex';
+  cardTitle.innerHTML = json[0].city;
+  textP.innerHTML = json[0].temperature;
+}
+
+function appendInfoFromWeb(json) {
+  card.style.display = 'flex';
+  cardTitle.innerHTML = json.name;
+  textP.innerHTML = json.main.temp;
 }
