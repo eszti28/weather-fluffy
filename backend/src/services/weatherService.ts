52609948585d4d1bd866d6ec293a62dbdef5a5cc
Eszter Csaki <esztercsaki3@gmail.com>
2022-11-23 10:22:43 +0100

    removed console.logs

--- backend/src/services/weatherService.ts
+++ backend/src/services/weatherService.ts
@@ -1,21 +1,19 @@
-import { DESTRUCTION } from 'dns';
 import { WeatherInfoDomainModel } from '../models/domain/WeatherInfoDomainModel';
 import { weatherRepository } from '../repositories/weatherRepository';
 
 export const weatherService = {
   async getWeatherInfo(city: string): Promise<WeatherInfoDomainModel[]> {
     const cityInfo = await weatherRepository.getWeatherInfo(city);
-    const minutes = this.timeSince(cityInfo[0].date);
+    const minutes = this.timeSince(cityInfo[0]?.date);
+
     console.log(minutes);
     if (cityInfo.length !== 0 && minutes < 10) {
-      console.log('works');
       return cityInfo;
     } else {
       return [];
     }
   },
   async addWeatherInfo(weatherData: any): Promise<void> {
-    console.log('gali');
     const mappedWeatherData = {
       date: weatherData.dt,
       city: weatherData.name,
@@ -28,6 +26,7 @@ export const weatherService = {
       windDeg: weatherData.wind.deg,
       clouds: weatherData.clouds.all,
     };
+
     await weatherRepository.addWeatherInfo(mappedWeatherData);
   },
 
