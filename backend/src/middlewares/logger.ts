a407649390ab8fea24d38159de5936c95765ef01
Eszter Csaki <esztercsaki3@gmail.com>
2022-11-23 14:33:44 +0100

    added try catch to logger

--- backend/src/middlewares/logger.ts
+++ backend/src/middlewares/logger.ts
@@ -1,6 +1,10 @@
 import * as fs from 'fs';
 
 export function logger(message: string) {
-  const textFile = fs.readFileSync('logger.txt');
-  fs.writeFileSync('logger.txt', textFile + `\n${message}`);
+  try {
+    const textFile = fs.readFileSync('logger.txt');
+    fs.writeFileSync('logger.txt', textFile + `\n${message}`);
+  } catch (err) {
+    console.log(err);
+  }
 }
